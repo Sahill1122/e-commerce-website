852a259704572c459e9b2c61c5efd05d84a1b2ca
Sahil Chauhan <sahilc200204@gmail.com>
2026-09-20 22:08:24 +0530

    update all categories

--- app.js
+++ app.js
@@ -7,3 +7,29 @@ close.addEventListener("click", () => {
 open.addEventListener("click", () => {
   menu.style.visibility = "visible";
 });
+
+
+
+
+const categoryFilter = document.querySelector("#categoryFilter");
+
+const products = document.querySelectorAll(".items");
+
+categoryFilter.addEventListener("change", () => {
+
+    const selectedCategory = categoryFilter.value;
+
+    products.forEach(product => {
+
+        if (
+            selectedCategory === "all" ||
+            product.dataset.category === selectedCategory
+        ) {
+            product.style.display = "block";
+        } else {
+            product.style.display = "none";
+        }
+
+    });
+
+});
